e8ca5ba60c783b5eb25ba4d6ae05d05332870507
Rifat <rifatsakib353@hmail.com>
2025-02-21 15:27:10 +0600

    error file added

---
+++ src/Pages/Shared/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 className='text-5xl flex flex-row justify-center items-center text-red-600 h-screen font-extrabold'>Error 404</h1>
+        </div>
+    );
+};
+
+export default Error;

82472f55383ee4783f75788d908dd2e9f8e02758
Rifat <rifatsakib353@hmail.com>
2025-02-21 15:41:36 +0600

    Hooks useaxiospublic done

---
+++ src/Hooks/UseAxiosPublic.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from "axios";
+
+const axiosPublic = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+const UseAxiosPublic = () => {
+    return axiosPublic;
+
+};
+
+export default UseAxiosPublic;
